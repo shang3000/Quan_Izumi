35ecb310c7f95ab6680264151db10e550f401d5b
不如吃茶去 <1625729527@qq.com>
2026-08-13 16:53:52 +0800

    更新practice文件

--- 爬虫/Practice.py
+++ 爬虫/Practice.py
@@ -1,10 +1,1 @@
-import requests
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36 Edg/148.0.0.0"
-}
-response = requests.get(url='https://movie.douban.com/top250', headers=headers)
-if response.ok:
-    print(response.text)
-else:
-    print("请求失败...")
+print("hello world")
